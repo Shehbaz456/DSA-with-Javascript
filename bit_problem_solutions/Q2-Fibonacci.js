d622235cd78eae2fd54708fc381f362a5cc60f98
shehbaz <mdshehbaz666666@gmail.com>
2025-10-21 13:20:34 +0530

    Refactor Fibonacci function implementations and comment out unused code

--- bit_problem_solutions/Q2-Fibonacci.js
+++ bit_problem_solutions/Q2-Fibonacci.js
@@ -5,29 +5,29 @@
 
 // Input: n = 3 -> Output: 2
 
-let Fibonacci = function (n) {
-   // way 1
-   //  let arr = [0,1]
-   //  for (let i = 2; i <= n; i++) {
-   //      arr.push(arr[i-1]+arr[i-2])
-   //  } 
-   //  return arr[n];
-   // way 2
-   if(n===0) return 0
-   if(n===1) return 1
-   let prev1 = 0
-   let prev2 = 1
-   let current;  
-   for (let i = 2; i <= n; i++) {
-    current = prev1+prev2;
-    prev1=prev2;
-    prev2=current;
-   }
-   return current;
-}
+// let Fibonacci = function (n) {
+//    // way 1
+//    //  let arr = [0,1]
+//    //  for (let i = 2; i <= n; i++) {
+//    //      arr.push(arr[i-1]+arr[i-2])
+//    //  } 
+//    //  return arr[n];
+//    // way 2
+//    if(n===0) return 0
+//    if(n===1) return 1
+//    let prev1 = 0
+//    let prev2 = 1
+//    let current;  
+//    for (let i = 2; i <= n; i++) {
+//     current = prev1+prev2;
+//     prev1=prev2;
+//     prev2=current;
+//    }
+//    return current;
+// }
 
-let element = Fibonacci(7);
-console.log(element);
+// let element = Fibonacci(7);
+// console.log(element);
 
 
 // Fibonacci Series - 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233 ...
@@ -48,3 +48,38 @@ console.log(element);
 
 // let element = Fibonacci(7);
 // console.log(element);
+
+
+// fibonacci series
+// 0 1 1 2 3 5 8 13 21 ...
+// function fibonacci(n) {
+//    if(n===0){
+//       return 0
+//    }
+//    if(n===1){
+//       return 1
+//    }
+//    let ans = fibonacci(n-1) + fibonacci(n-2);
+//    console.log(ans);
+//    return ans;
+// }
+
+// let n = 6
+// console.log(fibonacci(n)); // 6th index elem 3
+
+
+// function fibonacci(n) {
+//    let a=0,b=1;
+//    let arr = [];
+//    for (let i = 0; i < n; i++) {
+//       arr.push(a)
+//       let c = a + b;
+//       a=b
+//       b=c
+//    }
+//    return arr;
+// }
+
+// let n = 6
+// console.log(fibonacci(n)); // 3
+
